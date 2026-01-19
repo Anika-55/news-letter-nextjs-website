7cd7411cd89ad8c16d24c2ddb81bf1fc85948e44
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2026-01-19 15:21:18 +0600

    Impliement RSS Feeds section

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,12 @@
-// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help
-
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
