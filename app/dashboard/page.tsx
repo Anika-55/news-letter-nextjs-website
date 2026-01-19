64e2c079444caf51f9fbc22d5f5acdf55f5b651f
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2026-01-19 11:41:27 +0600

    Impliement dashboard header

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
 
 
-export default function page() {
+export default function DashboardPage() {
   return (
-    <div>page</div>
+    <div className="min-h-screen bg-gradient-to-b from-white to-gray-50 dark:from-black dark:to-gray-950">
+      <div className="container mx-auto py-12 px-6 lg:px-8 space-y-12">
+        Dashboard
+      </div>
+    </div>
   )
 }
